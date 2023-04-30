3ece3fe67c50ca4fe5b9e8e0876c212deda43999
Enrique Flores Ortiz <floresnenrique1@gmail.com>
2023-04-29 20:29:11 -0600

    Primer Commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const hbs = require('hbs');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.set('view engine','hbs');
+hbs.registerPartials(__dirname + '/views/partials',()=>{});
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+app.get('/login',(req,res)=>{
+    res.render('page-login');
+});
+
+app.get('/404',(req,res)=>{
+    res.render('page-error-404');
+});
+
+app.listen(port,()=>{
+    console.log('Serviodr corriendo en el puerto: ',port);
+  });
